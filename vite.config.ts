6712d0c3d3f91f5b7b781069832fb56dd5c7bdaa
akshayaj108 <akshayjadhao806@gmail.com>
2024-09-11 19:29:50 +0530

    vite port config

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0", // This binds the server to 0.0.0.0, allowing external access
+    port: process.env.PORT || 5173, // Use the environment port or default to 5173
+  },
 });
