164113eed40e9dde54cbcfbc2e8efddb6a2df8d7
Akshay Jadhao <akshayjadhao806@gmail.com>
2025-01-08 00:05:06 +0530

    add to cart, item price calculation and remove item from cart ui and functionality created

---
+++ src/components/OrderSummary.tsx
@@ -0,0 +1,62 @@
+import { CartItem } from "@/pages/DetailsPage";
+import { Restaurant } from "@/types";
+import { CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Separator } from "./ui/separator";
+import { Trash } from "lucide-react";
+
+type Props = {
+  restaurant: Restaurant;
+  cartItem: CartItem[];
+  removeCartItem: (cartItem: CartItem) => void;
+};
+
+const OrderSummary = ({ restaurant, cartItem, removeCartItem }: Props) => {
+  const getTotalCost = () => {
+    const totalInPence = cartItem.reduce(
+      (total, cartItem) => total + cartItem.price * cartItem.quantity,
+      0
+    );
+    const totalWithDelivery = totalInPence + restaurant.deliveryPrice;
+    return totalWithDelivery;
+  };
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold tracking-tight flex justify-between">
+          <span>Your Order</span>
+          <span>RS - {getTotalCost().toFixed(1)}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-5">
+        {cartItem.map((item) => (
+          <div className="flex justify-between">
+            <span>
+              <Badge variant="outline" className="mr-2">
+                {item.quantity}
+              </Badge>
+              {item.name}
+            </span>
+            <span className="flex items-center gap-1">
+              <Trash
+                className="cursor-pointer"
+                color="red"
+                size={20}
+                onClick={() => removeCartItem(item)}
+              />
+              RS - {(item.price * item.quantity).toFixed(1)}
+            </span>
+          </div>
+        ))}
+        <Separator />
+        <div className="flex justify-between">
+          <span>Delivery</span>
+          <span>RS - {restaurant.deliveryPrice.toFixed(2)}</span>
+        </div>
+        <Separator />
+      </CardContent>
+    </>
+  );
+};
+
+export default OrderSummary;
--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,18 +1,81 @@
 import { useGetRestaurantDetails } from "@/apiRequest/SearchRestaurantsApi";
 import MenuItem from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfoCard from "@/components/RestaurantInfoCard";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 function DetailsPage() {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurantDetails(restaurantId);
+  const [cartItem, setCartItem] = useState<CartItem[]>([]);
+
   if (isLoading) {
     return "Loading";
   }
   if (!restaurant) {
     return "Restaurant not found";
   }
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItem((prevCartItem) => {
+      //1. first check cart if item is already exits then update quantity.
+      const isExistItem = prevCartItem.find(
+        (cartItem: CartItem) => cartItem._id === menuItem._id
+      );
+      let updatedCartItem;
+      if (isExistItem) {
+        updatedCartItem = prevCartItem.map((cartItem: CartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItem = [
+          ...prevCartItem,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+      //2. if not then add item in cart as new item
+      return updatedCartItem;
+    });
+  };
+  const removeCartItem = (menuItem: CartItem) => {
+    setCartItem((prevCartItem) => {
+      const isExistItem = prevCartItem.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+      let updatedCartItem;
+      if (isExistItem?.quantity == 1) {
+        updatedCartItem = prevCartItem.filter(
+          (cartItem) => cartItem._id !== menuItem._id
+        );
+      } else {
+        updatedCartItem = prevCartItem.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? {
+                ...cartItem,
+                quantity: cartItem.quantity - 1,
+              }
+            : cartItem
+        );
+      }
+      return updatedCartItem;
+    });
+  };
   return (
     <div className="flex flex-col gap-10">
       <AspectRatio ratio={16 / 5}>
@@ -27,8 +90,20 @@ function DetailsPage() {
           <RestaurantInfoCard restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem) => (
-            <MenuItem menuItem={menuItem} />
+            <MenuItem
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
+        </div>
+        <div>
+          <Card>
+            <OrderSummary
+              restaurant={restaurant}
+              cartItem={cartItem}
+              removeCartItem={removeCartItem}
+            />
+          </Card>
         </div>
       </div>
     </div>
